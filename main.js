97fc8988c28c75b3b854375fe816b96e5cbe4f40
LazyCROW <hardikkardam21@gmail.com>
2021-09-12 16:10:27 +0530

    Sprites added

--- main.js
+++ main.js
@@ -5,6 +5,21 @@ let gwH = gameWindow.height;
 let gwW = gameWindow.width;
 let ctx = gameWindow.getContext("2d");
 
+let windowOK = false;
+
+// image assets count
+let total_assets = 0;
+const animation_data =  {
+    stance_frames: 1,
+    run_frames: 6,
+    bg_main: 1,
+    bg_parallax: 0
+}
+for(key in animation_data) {
+    total_assets += animation_data[key];
+}
+let currently_loaded = 0;
+
 // game sprite
 let runner = null;
 
@@ -17,37 +32,73 @@ if(windowHeight > windowWidth) {
     ctx.fillText("to landscape view", (gwW/2), (gwH/2) + 30);
 } else {
     if((windowHeight/windowWidth) < (16/9)) {
-        // gameWindow.className = 'isnot169';
         gameWindow.width = windowWidth * 0.9;
         gameWindow.height = windowWidth * 0.9 * 9 / 16;
-        console.log("not");
     } else {
-        // gameWindow.className = 'is169';
         gameWindow.height = windowHeight * 0.9;
         gameWindow.width = windowHeight * 0.9 * 16 / 9;
     }
-    // gameWindow.width = windowWidth * 0.9;
-    // gameWindow.height = windowHeight * 0.9;
     gwH = gameWindow.height;
     gwW = gameWindow.width;
 
-    runner = new Runner(gwW, gwH);
-    gameLoop();
+    // runner = new Runner(gwW, gwH);
+    // gameLoop();
+    windowOK = true;
 }
 
 var lastTime = 0;
+
+function startGame() {
+    if(windowOK) {
+        //for the runner
+        let spriteSheet = {
+            'stance': playerStanceSprite,
+            'run': playerRunSprite
+        };
+        runner = new Runner(gwW, gwH, spriteSheet);
+        gameLoop();
+    }
+}
 
 function gameLoop(timeStamp) {
     let deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, gwW, gwH);
+    ctx.drawImage(bgMain, 0, 0, gwW, gwH); 
     runner.update(deltaTime);
     runner.draw(ctx);
-    // for(let i = 0; i<10000; i++) {
-    //     for(let j=0; j<10000; j++) {
-
-    //     }
-    // }
     requestAnimationFrame(gameLoop);
+}
+
+
+// backgroud main
+var bgMain = new Image();
+bgMain.addEventListener('load', function() {
+    currently_loaded++;
+    startGame();
+}, false);
+bgMain.src = './assets/bg/Layer_0010_1.png';
+
+
+// background parallax
+
+// Load Stance img asset
+var playerStanceSprite = new Image();
+playerStanceSprite.addEventListener('load', function() {
+    currently_loaded++;
+    startGame();
+}, false);
+playerStanceSprite.src = './assets/stance/zabuza_stance.png';
+
+// Load Running animation img asset
+var playerRunSprite = [];
+for(let i=1; i<=6; i++) {
+    let frame = new Image();
+    frame.addEventListener('load', function() {
+        currently_loaded++;
+        startGame();
+    }, false);
+    frame.src = './assets/run/zabuza_run_frame'+i+'.png';
+    playerRunSprite.push(frame);
 }
--- runner.js
+++ runner.js
@@ -1,28 +1,34 @@
 class Runner {
-    constructor (gW, gH) {
+    constructor (gW, gH, spriteSheet) {
         this.height = gH/9;
         this.width = this.height/3;
         this.swordSwing = true;
-        this.swordHeight = this.height * 0.8;
-        this.swordWidth = this.height * 0.05;
+        this.runframe = 0;
+        this.runSprites = spriteSheet['run']
+        this.runMaxframe = this.runSprites.length;
+        this.runAnimationFrameDelay = 30; // adds delay
+        this.runAnimationFrameDelayCnt = 30;
+        // this.swordHeight = this.height * 0.8;
+        // this.swordWidth = this.height * 0.05;
         this.position = {
             x : 100,
             y : gH * 0.8
         };
         this.swordAngle = 225;
+        this.stanceSprite = spriteSheet['stance'];
     }
     draw(ctx) {
         ctx.fillStyle = '#0f0';
         ctx.setTransform(1, 0, 0, 1, 0, 0);
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        ctx.drawImage(this.runSprites[this.runframe], this.position.x, this.position.y);
+        if(this.runAnimationFrameDelayCnt <= 0) {
+            this.runframe = (this.runframe + 1 ) % this.runMaxframe;
+            this.runAnimationFrameDelayCnt = this.runAnimationFrameDelay;
+        } else {
+            this.runAnimationFrameDelayCnt--;
+        }
         if(this.swordSwing) {
-            ctx.translate(this.position.x, this.position.y);
-            ctx.rotate(this.swordAngle * Math.PI / 180);
-            ctx.fillStyle = '#f00';
-            ctx.fillRect(0 - this.width, 0, this.swordWidth, this.swordHeight);
-            if(this.swordAngle < 295) {
-                this.swordAngle++;
-            }
+            
         }
     }
     update(deltaTime) {
